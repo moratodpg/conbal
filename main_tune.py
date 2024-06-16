94f22808402dc6685461293dfd6d59c996047851
moratodpg <moratosotelo@gmail.com>
2024-06-16 21:07:43 +0200

    updated_workflow

--- main_tune.py
+++ main_tune.py
@@ -1,4 +1,6 @@
 from torch.utils.data import DataLoader, random_split, Subset
+import datetime
+import os
 import numpy as np
 import torch
 import torch.nn as nn
@@ -9,76 +11,46 @@ import json
 import wandb
 import yaml
 import pprint
+import argparse
 
-from models.dnn import SimpleNet, Trainer, MLP_dropout
+# os.environ["WANDB_MODE"] = "offline"
+
+from models.dnn import Trainer, MLP_dropout
 from datasets.buildings_dataset import Buildings
 from active_learning.active_learn import ActiveLearning
 
 def training_loop(config=None):
-    with wandb.init(config=config):
+    with wandb.init(config=config, mode="offline"):
 
         config = wandb.config
 
-        ## Input data
-        dataset_th_file = config.dataset
-
-        num_train = 70
-        num_test = 3000
-        num_epochs = 200
-        num_classes = 7
-
+        num_epochs = config.num_epochs
         batch_size = config.batch_size
         learning_rate = config.learning_rate
+        w_decay = config.weight_decay
         hidden_size = config.hidden_size
         layers = config.layers
+        num_forwards = config.num_forwards
         dropout_rate = config.dropout
-
-        mode = config.mode # "random"
         number_active_points = config.active_points
         num_active_iter = config.active_iterations
-        num_forwards = config.num_forwards
-        w_decay = config.weight_decay
+        mode = config.mode # "random"
+        save_interval = config.save_interval
 
-        filename = "score_stored"
-
-        # Assuming you have created a Buildings dataset instance named buildings_dataset
-        buildings_dataset = torch.load(dataset_th_file)
-
+        dataset_file = config.dataset
+        buildings_dataset = torch.load("datasets/" + dataset_file + ".pth")
+        print("Total number of samples in the dataset: ", len(buildings_dataset))
+        # Number of classes
+        num_classes = buildings_dataset.output_tensor.max().item() + 1
         # Load coordinates
-        coordinates = torch.load("datasets/build_24k_coord.pth")
-
-        # Calculate the length of the dataset
-        total_samples = len(buildings_dataset)
-        print("Total number of samples in the dataset: ", total_samples)
-
-        num_pool = total_samples - num_train - num_test
-
-        # Load location identifiers
-        # with open("datasets/subset_build_6kB_loc_identifier.json", 'r') as f:
-        #     loc_identifier = json.load(f)
-        # loc_identifier = torch.tensor(loc_identifier)
-
-        # Generate random indices for the downscaled dataset
-        # Ensure reproducibility with a fixed seed, if necessary
-        # np.random.seed(42)  # Uncomment to make the selection reproducible
-        # indices = np.random.choice(len(buildings_dataset), downscaled_size, replace=False)
-
-        ### Split the dataset into training, testing, and pool sets and store the indices
-        # Use random_split to split the dataset into training and testing sets
-        # train_ds, test_ds, pool_ds = random_split(buildings_dataset, [num_train, num_test, num_pool])
-
-        # # Storing the indices of the datasets
-        # subset_build = {}
-        # subset_build["train"] = train_ds.indices
-        # subset_build["test"] = test_ds.indices
-        # subset_build["pool"] = pool_ds.indices
-
-        # with open("datasets/subset_build_6kB_indices.json", 'w') as f:
-        #     json.dump(subset_build, f)
-
+        coordinates = torch.load("datasets/" + dataset_file + "_coord.pth")
         ### Load the indices
-        with open("datasets/build_24k_indices_0.json", 'r') as f:
+        with open("datasets/" + dataset_file + "_indices_0.json", 'r') as f:
             subset_build = json.load(f)
+
+        start_time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        results_dir = "results"
+        os.makedirs(results_dir, exist_ok=True)
 
         train_ds = Subset(buildings_dataset, subset_build["train"])
         test_ds = Subset(buildings_dataset, subset_build["test"])
@@ -94,9 +66,11 @@ def training_loop(config=None):
         # pool_ds_0 = pool_ds
 
         test_loader = DataLoader(test_ds, batch_size=batch_size, shuffle=False)
-
-        score_AL = []
         active_learn = ActiveLearning(num_active_points=number_active_points)
+
+        # Create dictionary with two keys: "accuracy_test" and "cost"
+        score_AL = {"accuracy_test": [], "cost": []}
+
 
         cost_total = 0
 
@@ -104,8 +78,6 @@ def training_loop(config=None):
             print("Active learning iteration: ", i)
             # Create DataLoaders
             train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
-            pool_loader = DataLoader(pool_ds, batch_size=len(pool_ds), shuffle=False)
-
 
             # Instantiate the classifier
             input_size = 384 # Only considering aerial images
@@ -115,18 +87,18 @@ def training_loop(config=None):
             trainer = Trainer(net, num_classes, train_loader, test_loader, criterion, optimizer, num_epochs, patience=400)
 
             trainer.train()
-            score_AL.append(trainer.score)
+            score_AL["accuracy_test"].append(trainer.score)
             wandb.log({"score": trainer.score})
             ## Loop
             idx_pool = pool_ds.indices
             idx_train = train_ds.indices
 
             trainer.model.load_state_dict(trainer.best_model)
-            
 
-            selected_idx_pool, cost_accum = active_learn.get_points(mode, trainer.model, num_forwards, buildings_dataset, idx_pool, coordinates)
-            wandb.log({"cost_accum": cost_accum})
-            cost_total += cost_accum
+            selected_idx_pool, cost = active_learn.get_points(mode, trainer.model, num_forwards, buildings_dataset, idx_pool, coordinates)
+            score_AL["cost"].append(cost)
+            wandb.log({"cost_accum": cost})
+            cost_total += cost
             
             ## Updated indices based on selected samples
             idx_pool_ = [idx for idx in idx_pool if idx not in selected_idx_pool]
@@ -135,29 +107,38 @@ def training_loop(config=None):
             ## Updated subdatasets based on selected samples
             train_ds = torch.utils.data.dataset.Subset(buildings_dataset, idx_train_) 
             pool_ds = torch.utils.data.dataset.Subset(buildings_dataset, idx_pool_) 
-
             print(len(pool_ds), len(train_ds))
 
-        # Storing results
-        filename_id = filename + ".json"
-        with open(filename_id, 'w') as f:
-            json.dump(score_AL, f)
+            if i % save_interval == 0:
+                model_filename = os.path.join(results_dir, f"{start_time}_{custom_name}.pth")
+                torch.save(trainer.model.state_dict(), model_filename)
+                print(f"Model saved: {model_filename}")
+                model_filename = os.path.join(results_dir, f"{start_time}_{custom_name}.json")
+                with open(model_filename, 'w') as f:
+                    json.dump(score_AL, f)
         
         # Storing the final model
-        torch.save(trainer.model.state_dict(), "final_model_stored.pth")
+        model_filename = os.path.join(results_dir, f"{start_time}_{custom_name}.pth")
+        torch.save(trainer.model.state_dict(), model_filename)
+        print(f"Model saved: {model_filename}")
+        model_filename = os.path.join(results_dir, f"{start_time}_{custom_name}.json")
+        with open(model_filename, 'w') as f:
+            json.dump(score_AL, f)
     
 if __name__ == "__main__":
-    config_file = "config/al_multiple.yaml"
-
+    parser = argparse.ArgumentParser(description="Training script with wandb sweep configuration.")
+    parser.add_argument("config_file", type=str, help="Path to the configuration file.")
+    parser.add_argument("filename", type=str, nargs='?', default="output", help="Custom name for the output files (optional).")
+    
+    args = parser.parse_args()
+    config_file = "config/" + args.config_file + ".yaml"
     # wandb.login()
-
     with open(config_file, "r") as file:
         config = yaml.safe_load(file)
+    custom_name = args.filename
     pprint.pprint(config)
-
     # Initialize the sweep
     sweep_id = wandb.sweep(config, project="storing_AL")
-
     # Run the sweep
     wandb.agent(sweep_id, function=training_loop, count=1)
 
